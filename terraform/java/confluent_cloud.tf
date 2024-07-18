1a7bf9e667fd0fdebc8436c3205a9375b44781a3
Eike Thaden <ethaden@confluent.io>
2024-07-18 12:30:34 +0000

    Made terraform setup more modular

--- terraform/java/confluent_cloud.tf
+++ terraform/java/confluent_cloud.tf
@@ -3,13 +3,40 @@
 # Set up a basic cluster (or a standard cluster, see below)
 resource "confluent_kafka_cluster" "example_kafka_lambda_cluster" {
   display_name = "${local.resource_prefix}_example_aws_kafka_lambda"
-  availability = "SINGLE_ZONE"
-  cloud        = "AWS"
+  availability = var.ccloud_cluster_availability
+  cloud        = var.ccloud_cluster_cloud_provider
   region       = var.ccloud_cluster_region
   # Use standard if you want to have the ability to grant role bindings on topic scope
   # standard {}
-  # For cost reasons, we use a basic cluster
-  basic {}
+  # For cost reasons, we use a basic cluster by default. However, you can choose a different type by setting the variable ccloud_cluster_type
+  # As each different type is represented by a unique block in the cluster resource, we use dynamic blocks here.
+  # Only exactly one can be active due to the way we've chosen the condition for "for_each"
+  dynamic "basic" {
+    for_each = var.ccloud_cluster_type=="basic" ? [true] : []
+    content {
+    }
+  }
+  dynamic "standard" {
+    for_each = var.ccloud_cluster_type=="standard" ? [true] : []
+    content {
+    }
+  }
+  dynamic "enterprise" {
+    for_each = var.ccloud_cluster_type=="enterprise" ? [true] : []
+    content {
+    }
+  }
+  dynamic "dedicated" {
+    for_each = var.ccloud_cluster_type=="dedicated" ? [true] : []
+    content {
+        cku = var.ccloud_cluster_ckus
+    }
+  }
+  dynamic "freight" {
+    for_each = var.ccloud_cluster_type=="freight" ? [true] : []
+    content {
+    }
+  }
 
   environment {
     id = var.ccloud_environment_id
@@ -20,7 +47,7 @@ resource "confluent_kafka_cluster" "example_kafka_lambda_cluster" {
   }
 }
 
-# Topic "test"
+# Topic with configured name
 resource "confluent_kafka_topic" "example_kafka_lambda_topic_test" {
   kafka_cluster {
     id = confluent_kafka_cluster.example_kafka_lambda_cluster.id
@@ -47,9 +74,10 @@ resource "confluent_service_account" "example_kafka_lambda_sa_cluster_admin" {
   description  = "Service Account AWS Kafka Lambda Example Cluster Admin"
 }
 
+# An API key with Cluster Admin access. Required for provisioning the cluster-specific resources such as our topic
 resource "confluent_api_key" "example_kafka_lambda_api_key_cluster_admin" {
   display_name = "${local.resource_prefix}_example_kafka_lambda_api_key_cluster_admin"
-  description  = "Kafka API Key that is owned by '${local.resource_prefix}_example_kafka_lambda_sa' service account"
+  description  = "Kafka API Key that is owned by '${local.resource_prefix}_example_kafka_lambda_sa_cluster_admin' service account"
   owner {
     id          = confluent_service_account.example_kafka_lambda_sa_cluster_admin.id
     api_version = confluent_service_account.example_kafka_lambda_sa_cluster_admin.api_version
@@ -71,6 +99,7 @@ resource "confluent_api_key" "example_kafka_lambda_api_key_cluster_admin" {
   }
 }
 
+# Assign the CloudClusterAdmin role to the cluster admin service account
 resource "confluent_role_binding" "example_kafka_lambda_role_binding_cluster_admin" {
   principal   = "User:${confluent_service_account.example_kafka_lambda_sa_cluster_admin.id}"
   role_name   = "CloudClusterAdmin"
@@ -110,21 +139,22 @@ resource "confluent_api_key" "example_kafka_lambda_api_key_producer" {
   }
 }
 
-# For role bindings such as DeveloperRead and DeveloperWrite at least a standard cluster type would be required. Let's use ACLs instead
-# resource "confluent_role_binding" "example_kafka_lambda_role_binding_producer" {
-#   principal   = "User:${confluent_service_account.example_kafka_lambda_sa_producer.id}"
-#   role_name   = "DeveloperWrite"
-#   # Role binding on topic level would require a standard cluster, but we want to use a basic cluster
-#   # crn_pattern = "${confluent_kafka_cluster.example_kafka_lambda_cluster.rbac_crn}/kafka=${confluent_kafka_cluster.example_kafka_lambda_cluster.id}/topic=${confluent_kafka_topic.example_kafka_lambda_topic_test.topic_name}"
-#   # Grant access to all topics instead
-#   crn_pattern = confluent_kafka_cluster.example_kafka_lambda_cluster.rbac_crn
-#   lifecycle {
-#     prevent_destroy = false
-#   }
-# }
+# For role bindings such as DeveloperRead and DeveloperWrite at least a standard cluster type would be required. We use ACLs instead for basic clusters
+resource "confluent_role_binding" "example_kafka_lambda_role_binding_producer" {
+  # Instaniciate this block only if the cluster type is NOT basic
+  count = var.ccloud_cluster_type=="basic" ? 0 : 1
+  principal   = "User:${confluent_service_account.example_kafka_lambda_sa_producer.id}"
+  role_name   = "DeveloperWrite"
+  crn_pattern = "${confluent_kafka_cluster.example_kafka_lambda_cluster.rbac_crn}/kafka=${confluent_kafka_cluster.example_kafka_lambda_cluster.id}/topic=${confluent_kafka_topic.example_kafka_lambda_topic_test.topic_name}"
+  lifecycle {
+    prevent_destroy = false
+  }
+}
 resource "confluent_kafka_acl" "example_kafka_lambda_acl_producer" {
- kafka_cluster {
-    id = confluent_kafka_cluster.example_kafka_lambda_cluster.id
+  # Instaniciate this block only if the cluster type IS basic
+  count = var.ccloud_cluster_type=="basic" ? 1 : 0
+  kafka_cluster {
+     id = confluent_kafka_cluster.example_kafka_lambda_cluster.id
   }
   rest_endpoint  = confluent_kafka_cluster.example_kafka_lambda_cluster.rest_endpoint
   resource_type = "TOPIC"
@@ -174,9 +204,24 @@ resource "confluent_api_key" "example_kafka_lambda_api_key_consumer" {
   }
 }
 
+# For role bindings such as DeveloperRead and DeveloperWrite at least a standard cluster type would be required. Let's use ACLs instead
+resource "confluent_role_binding" "example_kafka_lambda_role_binding_consumer" {
+  # Instaniciate this block only if the cluster type is NOT basic
+  count = var.ccloud_cluster_type=="basic" ? 0 : 1
+  principal   = "User:${confluent_service_account.example_kafka_lambda_sa_consumer.id}"
+  role_name   = "DeveloperRead"
+  crn_pattern = "${confluent_kafka_cluster.example_kafka_lambda_cluster.rbac_crn}/kafka=${confluent_kafka_cluster.example_kafka_lambda_cluster.id}/topic=${confluent_kafka_topic.example_kafka_lambda_topic_test.topic_name}"
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
 resource "confluent_kafka_acl" "example_kafka_lambda_acl_consumer" {
- kafka_cluster {
-    id = confluent_kafka_cluster.example_kafka_lambda_cluster.id
+  # Instaniciate this block only if the cluster type IS basic
+  count = var.ccloud_cluster_type=="basic" ? 1 : 0
+
+  kafka_cluster {
+     id = confluent_kafka_cluster.example_kafka_lambda_cluster.id
   }
   rest_endpoint  = confluent_kafka_cluster.example_kafka_lambda_cluster.rest_endpoint
   resource_type = "TOPIC"
@@ -196,7 +241,10 @@ resource "confluent_kafka_acl" "example_kafka_lambda_acl_consumer" {
 }
 
 resource "confluent_kafka_acl" "example_kafka_lambda_acl_consumer_group" {
- kafka_cluster {
+  # Instaniciate this block only if the cluster type IS basic
+  count = var.ccloud_cluster_type=="basic" ? 1 : 0
+
+  kafka_cluster {
     id = confluent_kafka_cluster.example_kafka_lambda_cluster.id
   }
   rest_endpoint  = confluent_kafka_cluster.example_kafka_lambda_cluster.rest_endpoint
@@ -216,19 +264,6 @@ resource "confluent_kafka_acl" "example_kafka_lambda_acl_consumer_group" {
   }
 }
 
-
-# For role bindings such as DeveloperRead and DeveloperWrite at least a standard cluster type would be required. Let's use ACLs instead
-# resource "confluent_role_binding" "example_kafka_lambda_role_binding_consumer" {
-#   principal   = "User:${confluent_service_account.example_kafka_lambda_sa_consumer.id}"
-#   role_name   = "DeveloperRead"
-#   # Role binding on topic level would require a standard cluster, but we want to use a basic cluster
-#   # crn_pattern = "${confluent_kafka_cluster.example_kafka_lambda_cluster.rbac_crn}/kafka=${confluent_kafka_cluster.example_kafka_lambda_cluster.id}/topic=${confluent_kafka_topic.example_kafka_lambda_topic_test.topic_name}"
-#   # Grant access to all topics instead
-#   crn_pattern = confluent_kafka_cluster.example_kafka_lambda_cluster.rbac_crn
-#   lifecycle {
-#     prevent_destroy = false
-#   }
-# }
 output "cluster_bootstrap_server" {
    value = confluent_kafka_cluster.example_kafka_lambda_cluster.bootstrap_endpoint
 }
@@ -236,24 +271,28 @@ output "cluster_rest_endpoint" {
     value = confluent_kafka_cluster.example_kafka_lambda_cluster.rest_endpoint
 }
 
-output "cluster_api_key_admin" {
-    value = nonsensitive("Key: ${confluent_api_key.example_kafka_lambda_api_key_cluster_admin.id}\nSecret: ${confluent_api_key.example_kafka_lambda_api_key_cluster_admin.secret}")
-}
+# The next entries demonstrate how to output the generated API keys to the console even though they are considered to be sensitive data by Terraform
+# Uncomment these lines if you want to generate that output
+# output "cluster_api_key_admin" {
+#     value = nonsensitive("Key: ${confluent_api_key.example_kafka_lambda_api_key_cluster_admin.id}\nSecret: ${confluent_api_key.example_kafka_lambda_api_key_cluster_admin.secret}")
+# }
 
-output "cluster_api_key_producer" {
-    value = nonsensitive("Key: ${confluent_api_key.example_kafka_lambda_api_key_producer.id}\nSecret: ${confluent_api_key.example_kafka_lambda_api_key_producer.secret}")
-}
+# output "cluster_api_key_producer" {
+#     value = nonsensitive("Key: ${confluent_api_key.example_kafka_lambda_api_key_producer.id}\nSecret: ${confluent_api_key.example_kafka_lambda_api_key_producer.secret}")
+# }
 
-output "cluster_api_key_consumer" {
-    value = nonsensitive("Key: ${confluent_api_key.example_kafka_lambda_api_key_consumer.id}\nSecret: ${confluent_api_key.example_kafka_lambda_api_key_consumer.secret}")
-}
+# output "cluster_api_key_consumer" {
+#     value = nonsensitive("Key: ${confluent_api_key.example_kafka_lambda_api_key_consumer.id}\nSecret: ${confluent_api_key.example_kafka_lambda_api_key_consumer.secret}")
+# }
 
-# Generate console client configuration files for testing
+# Generate console client configuration files for testing in subfolder "generated/client-configs"
+# PLEASE NOTE THAT THESE FILES CONTAIN SENSITIVE CREDENTIALS
 resource "local_sensitive_file" "client_config_files" {
-  for_each = {
+  # Do not generate any files if var.ccloud_cluster_generate_client_config_files is false
+  for_each = var.ccloud_cluster_generate_client_config_files ? {
     "admin" = confluent_api_key.example_kafka_lambda_api_key_cluster_admin,
     "producer" = confluent_api_key.example_kafka_lambda_api_key_producer,
-    "consumer" = confluent_api_key.example_kafka_lambda_api_key_consumer}
+    "consumer" = confluent_api_key.example_kafka_lambda_api_key_consumer} : {}
 
   content = templatefile("${path.module}/templates/client.conf.tpl",
   {
--- terraform/java/variables.tf
+++ terraform/java/variables.tf
@@ -67,14 +67,67 @@ variable "ccloud_environment_id" {
     description = "ID of the Confluent Cloud environment to use"
 }
 
+variable "ccloud_cluster_cloud_provider" {
+    type = string
+    default = "AWS"
+    description = "The cloud provider of the Confluent Cloud Kafka cluster"
+    validation {
+        condition = var.ccloud_cluster_cloud_provider=="AWS" || var.ccloud_cluster_cloud_provider=="AZURE" || var.ccloud_cluster_cloud_provider=="GCP"
+        error_message = "The cloud provider of the Confluent Cloud cluster must either by \"AWS\", \"AZURE\" or \"GCP\""
+    }
+}
+
+
 variable "ccloud_cluster_region" {
     type = string
     default = "us-east-2"
     description = "The region used to deploy the Confluent Cloud Kafka cluster"
 }
 
+variable "ccloud_cluster_type" {
+    type = string
+    default = "basic"
+    description = "The cluster type of the Confluent Cloud Kafka cluster. Valid values are \"basic\", \"standard\", \"dedicated\", \"enterprise\", \"freight\""
+    validation {
+        condition = var.ccloud_cluster_type=="basic" || var.ccloud_cluster_type=="standard" || var.ccloud_cluster_type=="dedicated" || var.ccloud_cluster_type=="enterprise" || var.ccloud_cluster_type=="freight"
+        error_message = "Valid Confluent Cloud cluster types are \"basic\", \"standard\", \"dedicated\", \"enterprise\""
+    }
+}
+
+variable "ccloud_cluster_availability" {
+    type = string
+    default = "SINGLE_ZONE"
+    description = "The availability of the Confluent Cloud Kafka cluster"
+    validation {
+        condition = var.ccloud_cluster_availability=="SINGLE_ZONE" || var.ccloud_cluster_availability=="MULTI_ZONE"
+        error_message = "The availability of the Confluent Cloud cluster must either by \"SINGLE_ZONE\" or \"MULTI_ZONE\""
+    }
+}
+
+variable "ccloud_cluster_ckus" {
+    type = number
+    default = 2
+    description = "The number of CKUs to use if the Confluent Cloud Kafka cluster is \"dedicated\"."
+    validation {
+        condition = var.ccloud_cluster_ckus>=2
+        error_message = "The minimum number of CKUs for a dedicated cluster is 2"
+    }
+}
+
 variable "ccloud_cluster_topic" {
     type = string
     default = "test"
     description = "The name of the Kafka topic to create and to subscribe to"
+}
+
+variable "ccloud_cluster_generate_client_config_files" {
+    type = bool
+    default = false
+    description = "Set to true if you want to generate client configs with the created API keys under subfolder \"generated/client-configs\""
+}
+
+variable "aws_lambda_function_name" {
+    type = string
+    default = "example_kafka_lambda"
+    description = "The name of the lambda function"
 }
